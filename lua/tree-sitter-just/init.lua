750a7e3516cc370b444dbb1fc744e968c4567271
Anshuman <amedhi@connect.ust.hk>
2021-09-19 14:24:52 +0800

    local install mode

--- lua/tree-sitter-just/init.lua
+++ lua/tree-sitter-just/init.lua
@@ -1,10 +1,34 @@
 local M = {}
 
-function M.setup()
+local is_windows
+if jit ~= nil then
+	is_windows = jit.os == "Windows"
+else
+	is_windows = package.config:sub(1, 1) == "\\"
+end
+local get_separator = function()
+	if is_windows then
+		return "\\"
+	end
+	return "/"
+end
+local join_paths = function(...)
+	local separator = get_separator()
+	return table.concat({ ... }, separator)
+end
+
+function M.setup(arg)
 	local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
 	parser_config.just = {
 		install_info = {
-			url = "https://github.com/IndianBoy42/tree-sitter-just", -- local path or git repo
+			url = arg["local"] and join_paths(
+				vim.fn.stdpath("data"),
+				"site",
+				"pack",
+				"packer",
+				"start",
+				"tree-sitter-just"
+			) or "https://github.com/IndianBoy42/tree-sitter-just", -- local path or git repo
 			files = { "src/parser.c", "src/scanner.cc" },
 			branch = "main",
 		},
