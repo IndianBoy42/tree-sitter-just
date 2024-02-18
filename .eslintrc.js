1b8b11df7fb11dcea3e81a11333333bc4b5f5d81
Trevor Gross <tmgross@umich.edu>
2024-02-18 18:08:53 -0500

    Apply formatting from Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,25 @@
 module.exports = {
-  "env": {
-    "commonjs": true,
-    "es2021": true,
+  env: {
+    commonjs: true,
+    es2021: true,
   },
   // NOTE: eslint is deprecating its formatting rules so eslint-config-prettier
   // will not always be necessary.
-  "extends": ["google", "prettier"],
-  "overrides": [],
-  "parserOptions": {
-    "ecmaVersion": "latest",
-    "sourceType": "module",
+  extends: ["google", "prettier"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-  "rules": {
-    "indent": ["error", 2, { "SwitchCase": 1 }],
+  rules: {
+    indent: ["error", 2, { SwitchCase: 1 }],
     "max-len": [
       "error",
       {
-        "code": 120,
-        "ignoreComments": true,
-        "ignoreUrls": true,
-        "ignoreStrings": true,
+        code: 120,
+        ignoreComments: true,
+        ignoreUrls: true,
+        ignoreStrings: true,
       },
     ],
     "valid-jsdoc": 0,
