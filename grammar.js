65ab27db69a57050384e8739843bf81461e35893
Mikhail Katychev <mkatych@gmail.com>
2024-02-22 12:39:10 -0600

    apply suggestion

--- grammar.js
+++ grammar.js
@@ -274,7 +274,7 @@ module.exports = grammar({
       seq(
         $._indent,
         optional(seq($.shebang, $._newline)),
-        repeat(seq($.recipe_line, $._newline)),
+        repeat(choice(seq($.recipe_line, $._newline), $._newline)),
         $._dedent,
       ),
 
