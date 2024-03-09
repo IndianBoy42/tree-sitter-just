ecc15dba96748279b40f46f2997a7658eeb5d7fc
Trevor Gross <tmgross@umich.edu>
2024-03-09 02:43:38 -0500

    Set a default injection language for inline commands

--- queries-src/injections.scm
+++ queries-src/injections.scm
@@ -3,6 +3,18 @@
 
 (comment) @comment
 
+; Default everything to be bash
+; FIXME: uncomment this when we have a better idea of how precedence will work
+; in all editors, since this is pretty turbulant in early 2024.
+; (recipe_body
+;   (#set! injection.language "bash")
+;   (#set! injection.include-children)) @injection.content
+
+(external_command
+  (command_body) @injection.content
+  (#set! injection.language "bash"))
+
+; Set highlighting for recipes that set a language
 (recipe_body
   (shebang (language) @injection.language)
   (#set! injection.include-children)) @injection.content
@@ -10,13 +22,3 @@
 ; Highlight the RHS of `=~` as regex
 ((regex_literal (_) @injection.content)
   (#set! injection.language "regex"))
-
-; Defaults if language is not set elsewhere
-
-; (recipe_line
-;   (text) @injection.content
-;   (#set! injection.language "bash"))
-
-; (external_command
-;   (command_body) @injection.content
-;   (#set! injection.language "bash"))
