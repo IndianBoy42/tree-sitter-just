0ddbe48fc4edbb27238fb48b9f121dbb383c5a91
Trevor Gross <tmgross@umich.edu>
2024-03-09 03:46:13 -0500

    Add support for setting global injection language via `set`
    Listen to the `set := [...]` configuration option to provide a default
    language. Otherwise, default to bash. This works great tested via nvim.
    
    Unfortunately, this does not work in Helix due to precedence differences
    :(. Hopefully they align theirs with NeoVim soon.
    
    Fixes #71

--- queries-src/injections.scm
+++ queries-src/injections.scm
@@ -1,24 +1,73 @@
+; FIXME: these are not compatible with helix due to precedence
+
+; ================ Always applicable ================
+
 ((comment) @injection.content
   (#set! injection.language "comment"))
 
 (comment) @comment
 
+; Highlight the RHS of `=~` as regex
+((regex_literal (_) @injection.content)
+  (#set! injection.language "regex"))
+
+; ================ Global defaults ================
+
 ; Default everything to be bash
-; FIXME: uncomment this when we have a better idea of how precedence will work
-; in all editors, since this is pretty turbulant in early 2024.
-; (recipe_body
-;   (#set! injection.language "bash")
-;   (#set! injection.include-children)) @injection.content
+(recipe_body
+  (#set! injection.language "bash")
+  (#set! injection.include-children)) @injection.content
 
 (external_command
   (command_body) @injection.content
   (#set! injection.language "bash"))
 
-; Set highlighting for recipes that set a language
+; ================ Global language specified ================
+; Global language is set with something like one of the following:
+;
+;    set shell := ["bash", "-c", ...]
+;    set shell := ["pwsh.exe"]
+;
+; We can extract the first item of the array, but we can't extract the language
+; name from the string with something like regex. So instead we special case
+; two things: powershell, which is likely to come with a `.exe` attachment that
+; we need to strip, and everything else which hopefully has no extension. We
+; separate this with a `#match?`.
+;
+; Unfortunately, there also isn't a way to allow arbitrary nesting or
+; alternatively set "global" capture variables. So we can set this for item-
+; level external commands, but not for e.g. external commands within an
+; expression without getting _really_ annoying. Should at least look fine since
+; they default to bash. Limitations...
+; See https://github.com/tree-sitter/tree-sitter/issues/880 for more on that.
+
+(source_file
+  (setting "shell" ":=" "[" (string) @langstr
+    (#match? @langstr ".*(powershell|pwsh|cmd).*")
+    (#set! injection.language "powershell"))
+  [
+    (recipe
+      (recipe_body (#set! injection.include-children)) @injection.content)
+
+    (assignment
+      (expression (value (external_command (command_body) @injection.content))))
+  ])
+
+(source_file
+  (setting "shell" ":=" "[" (string) @injection.language
+    (#not-match? @langstr ".*(powershell|pwsh|cmd).*"))
+  [
+    (recipe
+      (recipe_body (#set! injection.include-children)) @injection.content)
+
+    (assignment
+      (expression (value (external_command (command_body) @injection.content))))
+  ])
+
+; ================ Recipe language specified ================
+
+; Set highlighting for recipes that specify a language
 (recipe_body
   (shebang (language) @injection.language)
   (#set! injection.include-children)) @injection.content
 
-; Highlight the RHS of `=~` as regex
-((regex_literal (_) @injection.content)
-  (#set! injection.language "regex"))
